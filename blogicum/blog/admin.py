c6ff59995277c614c5fa37b7571cd288d6131923
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-11-10 17:12:14 +0300

    some changes

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Post, Category, Location, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'text', 'pub_date',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'description',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+admin.site.register(Comment)
+
+
+admin.site.empty_value_display = 'Не задано'
